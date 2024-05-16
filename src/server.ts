b107cbf2f9677d03e7bb82300fffda6a3412b95b
saikat <tawhidulislam3482@gmail.com>
2024-05-16 10:03:56 +0600

    express typescript mongoose setup

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    await mongoose.connect(process.env.DATABASE_URL);
   
   }
 
