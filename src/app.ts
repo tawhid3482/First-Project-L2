c054320042c9266038249f338223325aa9e22596
saikat <tawhidulislam3482@gmail.com>
2024-05-16 17:31:38 +0600

    eslint json

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import express, { Application, Request, Response } from 'express'
-const app:Application = express()
-const port = 3000
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
 
-app.get('/', (req:Request, res:Response ) => {
-  res.send('Hello World!')
-})
+// parser
+app.use(express.json());
+app.use(cors());
 
-export default app
+app.get("/", (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
 
+export default app;
