34cbfae5733fdcf30f42d9d1dc11cbed2244b72e
saikat <tawhidulislam3482@gmail.com>
2024-05-16 10:33:32 +0600

    server setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
+import express, { Application, Request, Response } from 'express'
+const app:Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req:Request, res:Response ) => {
   res.send('Hello World!')
 })
 
+export default app
 
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose'
+import config from "./app/config";
+import app from './app';
+
 
 async function main() {
-    await mongoose.connect(process.env.DATABASE_URL);
-  
-  }
+    try {
+        await mongoose.connect(config.database_url as string);
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
+        })
+    } catch (err) {
+        console.log(err)
+    }
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+
+}
+
