5b957ecc1237b5baca2c54682b17a3fe151ae43c
saikat <tawhidulislam3482@gmail.com>
2024-05-17 15:51:37 +0600

    student interface mongoose

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -3,16 +3,22 @@ import { Student } from './student/student.interface';
 
 const StudentSchema = new Schema<Student>({
   id: { type: String },
-  name:{
-    fristName:{
-        type:String,
-         required:true
+  name: {
+    fristName: {
+      type: String,
+      required: true,
     },
-    middleName:{
-        type:String,
-        required:true
-    }
-
-  }
-
+    middleName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    gender: ['male', 'female'],
+    birthday: { type: String },
+    email: { type: String },
+    avatar: { type: String },
+    contact: { type: String },
+  },
 });
